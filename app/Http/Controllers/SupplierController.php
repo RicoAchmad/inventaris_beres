32db1dbfe28ad646b041837120d0c7b768ec8cda
RicoAchmad <rico_250@smkassalaambandung.sch.id>
2022-03-26 10:53:34 +0700

    Projek Beres

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Alert;
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $supplier = Supplier::orderBy('id', 'desc')->get();
+        return view('admin.supplier.index', compact('supplier'));
+
+        // return response()->json([
+        //     'success' => true,
+        //     'message' => 'List Data Supplier',
+        //     'date' => $supplier
+        // ], 200);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('admin.supplier.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        alert()->error('Mohon maaf', 'Nomor Minimal 10 Digit dan maksimal 14 digit');
+
+        $validated = $request->validate([
+            'nama_supplier' => 'required',
+            'alamat' => 'required',
+            'no_wa' => 'required|min:10|max:14',
+        ]);
+
+        $supplier = new Supplier;
+        $supplier->nama_supplier = $request->nama_supplier;
+        $supplier->alamat = $request->alamat;
+        $supplier->no_wa = $request->no_wa;
+        $supplier->save();
+        Alert::success('Mantap', 'Data berhasil ditambah');
+        return redirect()->route('supplier.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Supplier  $supplier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Supplier $supplier)
+    {
+        //
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Supplier  $supplier
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $supplier = Supplier::findOrFail($id);
+        return view('admin.supplier.edit', compact('supplier'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Supplier  $supplier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $validated = $request->validate([
+            'nama_supplier' => 'required',
+            'alamat' => 'required',
+            'no_wa' => 'required',
+        ]);
+
+        $supplier = Supplier::findOrFail($id);
+        $supplier->nama_supplier = $request->nama_supplier;
+        $supplier->alamat = $request->alamat;
+        $supplier->no_wa = $request->no_wa;
+        $supplier->save();
+        Alert::success('Mantap', 'Data berhasil update');
+        return redirect()->route('supplier.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Supplier  $supplier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        if (!Supplier::destroy($id)) {
+            return redirect()->back();
+        }
+        Alert::success('Mantap', 'Data berhasil dihapus');
+        return redirect()->route('supplier.index');
+    }
+}
---
+++ resources/views/admin/barang/create.blade.php
@@ -0,0 +1,71 @@
+@extends('adminlte::page')
+
+@section('title','Tambah Barang')
+
+@section('content_header')
+
+<br>
+@endsection
+
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Input Barang</div>
+                <div class="card-body">
+                   <form action="{{route('barang.store')}}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                             <label for="">Nama Barang</label>
+                            <input type="text" name="nama_barang" class="form-control @error('nama_barang') is-invalid @enderror">
+                             @error('nama_barang')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                             <label for="">Kategori Barang</label>
+                            <input type="text" name="kategori_brg" class="form-control @error('kategori_brg') is-invalid @enderror">
+                             @error('kategori_brg')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Supplier</label>
+                            <select name="id_supplier" class="form-control @error('id_supplier') is-invalid @enderror" >
+                                @foreach($supplier as $data)
+                                    <option value="{{$data->id}}">{{$data->nama_supplier}}</option>
+                                @endforeach
+                            </select>
+                            @error('id_supplier')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        
+                            <button type="submit" class="btn btn-outline-primary">Simpan</button>
+                        </div>
+                   </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('js')
+
+@endsection
